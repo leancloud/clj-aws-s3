09e8c9ff79b7122d777e06de3a3a721c0f5e4ae3
paomian <qtang@leancloud.rocks>
2019-01-04 16:02:29 +0800

    :bug: fix clojure version

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/weavejester/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.11.359"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.359"
+                  :exclusions [joda-time commons-logging]]
+                 [com.amazonaws/aws-java-sdk-cloudwatch "1.11.359"]
                  [clj-time "0.6.0"]]
   :plugins [[codox "0.8.10"]])
